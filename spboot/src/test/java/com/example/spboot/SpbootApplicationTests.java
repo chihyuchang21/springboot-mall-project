fe85ed0480c47ab2d1adfceb7d258973cd84c523
Chih-Yu Chang <chihyuchang21@gmail.com>
2023-12-14 12:53:21 +0800

    初始化Spring Boot Project

---
+++ spboot/src/test/java/com/example/spboot/SpbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
